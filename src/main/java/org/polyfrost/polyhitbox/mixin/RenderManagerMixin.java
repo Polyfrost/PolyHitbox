66d88d39d74fcc411b5ecca18f27ca0260d189b6
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-08-04 09:41:55 +0800

    redirect debugBoundingBox

--- src/main/java/org/polyfrost/polyhitbox/mixin/RenderManagerMixin.java
+++ src/main/java/org/polyfrost/polyhitbox/mixin/RenderManagerMixin.java
@@ -2,10 +2,12 @@ package org.polyfrost.polyhitbox.mixin;
 
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import org.polyfrost.polyhitbox.config.ModConfig;
 import org.polyfrost.polyhitbox.hooks.MixinHooksKt;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderManager.class)
@@ -16,5 +18,10 @@ public abstract class RenderManagerMixin {
         if (MixinHooksKt.overrideHitbox(entity, x, y, z, partialTicks)) {
             callbackInfo.cancel();
         }
+    }
+
+    @Redirect(method = "doRenderEntity", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/RenderManager;debugBoundingBox:Z"))
+    private boolean redirectBoundingBox(RenderManager instance) {
+        return ModConfig.INSTANCE.enabled || instance.isDebugBoundingBox();
     }
 }
--- src/main/kotlin/org/polyfrost/polyhitbox/PolyHitbox.kt
+++ src/main/kotlin/org/polyfrost/polyhitbox/PolyHitbox.kt
@@ -27,22 +27,6 @@ object PolyHitbox {
         EventManager.INSTANCE.register(this)
     }
 
-    private var lastEnabled = false
-
-    @Subscribe
-    fun onTick(event: TickEvent) {
-        if (event.stage != Stage.END) return
-
-        if (ModConfig.enabled) {
-            mc.renderManager.isDebugBoundingBox = true
-        }
-        if (lastEnabled == ModConfig.enabled) return
-        if (lastEnabled) {
-            mc.renderManager.isDebugBoundingBox = false
-        }
-        lastEnabled = ModConfig.enabled
-    }
-
     var keybindToggled = false
     private var keybindLastPressed = false
 
